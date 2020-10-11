2332d8e97db460533c4e3aca67327ade033d5a1f
Courgette <courgett3@gmail.com>
2020-10-10 22:14:42 -0500

    Complete styling

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
-const Player = (_name, _symbol) => {
+const Player = (_name, _symbol, _color) => {
     const getName = () => _name;
 
     const getSymbol = () => _symbol;
 
+    const getSymbolColor = () => _color;
+
     return {
         getName, 
         getSymbol,
+        getSymbolColor,
     }
 };
 
@@ -18,8 +21,8 @@ const gameBoard = (() => {
         tie: 0
     }
     let _board = new Array(9).fill('');
-    let _player1 = Player('xx', 'x');
-    let _player2 = Player('oo', 'o');
+    let _player1 = Player('xx', 'x', '#55acee');
+    let _player2 = Player('oo', 'o', '#fa743e');
 
     const equal3 = (a, b, c) => {
         return (a === b && a === c) && a !== '';
@@ -125,21 +128,17 @@ const gameBoard = (() => {
         _board[position] = symbol;
     }
 
-    const getHumanSymbol = () => {
-        return _HUMAN_SYMBOL;
-    }
+    const getHumanSymbol = () => _HUMAN_SYMBOL;
 
-    const getAISymbol = () => {
-        return _AI_SYMBOL;
-    }
+    const getAISymbol = () => _AI_SYMBOL;
 
-    const getPlayer1Symbol = () => {
-        return _player1.getSymbol();
-    }
+    const getPlayer1Symbol = () => _player1.getSymbol();
 
-    const getPlayer2Symbol = () => {
-        return _player2.getSymbol();
-    }
+    const getPlayer2Symbol = () => _player2.getSymbol();
+
+    const getPlayer1SymbolColor = () => _player1.getSymbolColor();
+
+    const getPlayer2SymbolColor = () => _player2.getSymbolColor();
 
     const clearBoard = () => {
         _board = new Array(9).fill('');
@@ -153,18 +152,27 @@ const gameBoard = (() => {
         getAISymbol,
         getPlayer1Symbol,
         getPlayer2Symbol,
+        getPlayer1SymbolColor,
+        getPlayer2SymbolColor,
         clearBoard,
     }
 })();
 
 const displaySquaresText = (() => {
     const _AI_SYMBOL = gameBoard.getAISymbol();
+    const _AI_SYMBOL_COLOR = '#55acee';
     const _HUMAN_SYMBOL = gameBoard.getHumanSymbol();
+    const _HUMAN_SYMBOL_COLOR = '#fa743e';
     const _PLAYER1_SYMBOL = gameBoard.getPlayer1Symbol();
+    const _PLAYER1_SYMBOL_COLOR = gameBoard.getPlayer1SymbolColor();
     const _PLAYER2_SYMBOL = gameBoard.getPlayer2Symbol();
+    const _PLAYER2_SYMBOL_COLOR = gameBoard.getPlayer2SymbolColor();
+    let player1Turn = true;
+
     let _squares = Array.from(document.querySelectorAll('.square'));
     let _winner = document.querySelector('#winner'); 
-    let player1Turn = true;
+    let _turn = document.querySelector('#turn');
+    let _thinkingIcon = document.querySelector('#thinkingIcon');
 
     const runAIMode = () => {
         _squares.forEach(square => square.addEventListener('click', updateBoardAIMode));
@@ -180,22 +188,45 @@ const displaySquaresText = (() => {
             return;
         } else {
             event.target.textContent = _HUMAN_SYMBOL;
+            event.target.style.color = _HUMAN_SYMBOL_COLOR;
             gameBoard.humanMove(event.target.getAttribute('id'), _HUMAN_SYMBOL);
+            _turn.textContent = 'X\'s turn';
+            _turn.style.backgroundColor = '#55acee';
         }
 
         winner = gameBoard.checkWinner();
-        if (winner !== null) {
-            _winner.textContent = winner + ' is winner';
+        if (winner === 'o') {
+            _winner.textContent = 'Nice, ' + winner.toUpperCase() + '!';
+            _winner.style.color = '#fa743e';
             return;
-        } else {
+        } else if (winner === 'x') {
+            _winner.textContent = 'Nice, ' + winner.toUpperCase() + '!';
+            _winner.style.color = '#55acee';
+            return;
+        } else if (winner === 'tie') {
+            _winner.textContent = 'Tie!';
+            _winner.style.color = 'black';
+            return;
+        } else { 
             let bestPosition = gameBoard.aiMove();
-            if (bestPosition !== -1)
+            if (bestPosition !== -1) {
                 _squares[bestPosition].textContent = _AI_SYMBOL;
+                _squares[bestPosition].style.color = _AI_SYMBOL_COLOR;
+                _turn.textContent = 'O\'s turn';
+                _turn.style.backgroundColor = '#fa743e';
+            }
         }
 
         winner = gameBoard.checkWinner();
-        if (winner !== null) {
-            _winner.textContent = winner + ' is winner';
+        if (winner === 'o') {
+            _winner.textContent = 'Nice, ' + winner.toUpperCase() + '!';
+            _winner.style.color = '#fa743e';
+        } else if (winner === 'x') {
+            _winner.textContent = 'Nice, ' + winner.toUpperCase() + '!';
+            _winner.style.color = '#55acee';
+        } else if (winner === 'tie') {
+            _winner.textContent = 'Tie!';
+            _winner.style.color = 'black';
         }
     }
 
@@ -216,17 +247,30 @@ const displaySquaresText = (() => {
             if (player1Turn) {
                 symbol = _PLAYER1_SYMBOL;
                 player1Turn = false;
+                event.target.style.color = _PLAYER1_SYMBOL_COLOR;
+                _turn.textContent = 'O\'s turn';
+                _turn.style.backgroundColor = '#fa743e';
             } else {
                 symbol = _PLAYER2_SYMBOL;
                 player1Turn = true;
+                event.target.style.color = _PLAYER2_SYMBOL_COLOR;
+                _turn.textContent = 'X\'s turn';
+                _turn.style.backgroundColor = '#55acee';
             }
             event.target.textContent = symbol;
             gameBoard.humanMove(event.target.getAttribute('id'), symbol);
         } 
         
         winner = gameBoard.checkWinner();
-        if (winner !== null) {
-            _winner.textContent = winner + ' is winner';
+        if (winner === 'o') {
+            _winner.textContent = 'Nice, ' + winner.toUpperCase() + '!';
+            _winner.style.color = '#fa743e';
+        } else if (winner === 'x') {
+            _winner.textContent = 'Nice, ' + winner.toUpperCase() + '!';
+            _winner.style.color = '#55acee';
+        } else if (winner === 'tie') {
+            _winner.textContent = 'Tie!';
+            _winner.style.color = 'black';
         }
     }
 
@@ -246,25 +290,31 @@ const displaySquaresText = (() => {
 const setupGame = (() => {
     let _switchPlayMode = document.querySelector('#toggle');
     let _resetButton = document.querySelector('#resetBtn');
+    let _modeText = document.querySelector('#modeText');
+    let _winner = document.querySelector('#winner'); 
+    let _turn = document.querySelector('#turn');
 
     const initSwitchPlayMode = () => {
-        if(_switchPlayMode.checked) {
-            displaySquaresText.runAIMode();
-        } else {
-            displaySquaresText.runPlayerMode();
-        }
+        resetGame();
         _switchPlayMode.addEventListener('change', resetGame);
     }
 
     const resetGame = () => {
         gameBoard.clearBoard();
         displaySquaresText.clearSquares();
+        _winner.textContent = '';
         if(_switchPlayMode.checked) {
+            _modeText.textContent = 'Human vs. AI';
             displaySquaresText.removePlayerMode();
             displaySquaresText.runAIMode();
+            _turn.textContent = 'O\'s turn';
+            _turn.style.backgroundColor = '#fa743e';
         } else {
+            _modeText.textContent = 'Human vs. Human';
             displaySquaresText.removeAIMode();
             displaySquaresText.runPlayerMode();
+            _turn.textContent = 'X\'s turn';
+            _turn.style.backgroundColor = '#55acee';
         }
     }
 
